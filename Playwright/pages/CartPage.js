46ea32ada632d78f91a835a61c2e14ebf7b91443
AnastasiaM <nassty.ru@gmail.com>
2026-02-02 16:53:59 +0300

    transferring e2e.spec and api.spec test files to Playwright folder

---
+++ Playwright/pages/CartPage.js
@@ -0,0 +1,22 @@
+// Страница корзины
+import { expect } from "playwright/test";
+
+export class CartPage {
+    constructor(page) {
+
+        this.page = page;
+        this.checkoutnButton = page.locator('[data-test="checkout"]');
+        this.continueShoppingButton = page.locator('[data-test="continue-shopping"]');
+        this.listItems = page.locator('[data-test="cart-list"]'); //  список добавленных товаров
+    }
+ 
+    async itemList(){
+        await expect(this.listItems).toBeVisible();
+    }
+
+    async goToCheckout(){
+        await this.checkoutnButton.click();
+        await expect(this.page).toHaveURL("https://www.saucedemo.com/checkout-step-one.html");
+    }
+   
+}
